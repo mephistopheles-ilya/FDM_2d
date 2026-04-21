68e4ab4e5604d220fd8f32bb9b9a388cda0cf78c
mephistopheles_ilya <ilyuxa.popoff@yandex.ru>
2026-04-21 18:52:04 +0300

    solver oprimizations

--- solve.hpp
+++ solve.hpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <stdio.h>
+#include <immintrin.h>
 
 inline double min_division = 1e-32;
 
@@ -37,13 +38,48 @@ class linear_solver
 
   double dot (const double *vec1, const double *vec2)
   {
-    double s = 0;
-    for (unsigned int i = 0; i < n; ++i)
+    double sum = 0;
+    double res256[] = {0, 0, 0, 0};
+    unsigned int i = 0;
+    __m256d sum0 = _mm256_setzero_pd ();
+    __m256d sum1 = _mm256_setzero_pd ();
+    __m256d sum2 = _mm256_setzero_pd ();
+    __m256d sum3 = _mm256_setzero_pd ();
+    for (i = 0; i <= n - 16; i += 16)
       {
-        s += vec1[i] * vec2[i];
+        __m256d v1_0 = _mm256_loadu_pd (vec1 + i);
+        __m256d v2_0 = _mm256_loadu_pd (vec2 + i);
+        __m256d prod0 = _mm256_mul_pd (v1_0, v2_0);
+        sum0 = _mm256_add_pd (sum0, prod0);
+
+        __m256d v1_1 = _mm256_loadu_pd (vec1 + i + 4);
+        __m256d v2_1 = _mm256_loadu_pd (vec2 + i + 4);
+        __m256d prod1 = _mm256_mul_pd (v1_1, v2_1);
+        sum1 = _mm256_add_pd (sum1, prod1);
+
+        __m256d v1_2 = _mm256_loadu_pd (vec1 + i + 8);
+        __m256d v2_2 = _mm256_loadu_pd (vec2 + i + 8);
+        __m256d prod2 = _mm256_mul_pd (v1_2, v2_2);
+        sum2 = _mm256_add_pd (sum2, prod2);
+
+        __m256d v1_3 = _mm256_loadu_pd (vec1 + i + 12);
+        __m256d v2_3 = _mm256_loadu_pd (vec2 + i + 12);
+        __m256d prod3 = _mm256_mul_pd (v1_3, v2_3);
+        sum3 = _mm256_add_pd (sum3, prod3);
       }
-    return s;
-  }
+
+    __m256d total1 = _mm256_add_pd (sum0, sum1);
+    __m256d total2 = _mm256_add_pd (sum1, sum2);
+    __m256d total = _mm256_add_pd (total1, total2);
+
+    for (;i < n; ++i)
+      {
+        sum += vec1[i] * vec2[i];
+      }
+    _mm256_storeu_pd (res256, total);
+
+    return sum + res256[0] + res256[1] + res256[2] + res256[3];
+}
 
   void mult_sub_vec (double *res, const double *vec1, double w, const double *vec2)
   {
@@ -52,6 +88,31 @@ class linear_solver
         res[i] = vec1[i] - w * vec2[i];
       }
   }
+  void linear_combination (double *u, double *p, const double *r, const double *q, double betta)
+  {
+    double b2 = betta * betta;
+    for (unsigned int i = 0; i < n; ++i)
+      {
+        double bq = betta * q[i];
+        double res = r[i] + bq;
+        u[i] = res;
+        p[i] = res + bq + b2 * p[i];
+      }
+  }
+ void add_vec (double *res, const double *vec)
+ {
+   for (unsigned int i = 0; i < n; ++i)
+    {
+      res[i] += vec[i];
+    }
+ }
+ void add_vec (double *res, const double *vec, double alpha)
+ {
+   for (unsigned int i = 0; i < n; ++i)
+    {
+      res[i] += alpha * vec[i];
+    }
+ }
 
   void copy_vec (double *dest, const double *source)
   {
@@ -63,14 +124,10 @@ class linear_solver
 
   void apply_precond_J (const double *A, double *vec)
   {
-    (void) A;
-    (void) vec;
-#if 0
     for (unsigned int i = 0; i < n; ++i)
       {
         vec[i] /= A[i];
       }
-#endif
   }
 
 
@@ -100,7 +157,6 @@ public:
   {
     mat_mult_vec (A, I, x, Avec);
     mult_sub_vec (r, b, 1, Avec);
-    apply_precond_J (A, r);
     copy_vec (p, r);
     copy_vec (u, r);
     copy_vec (z, r);
@@ -109,33 +165,32 @@ public:
 
     double r_norm = 0;
     unsigned int it = 0;
+    double rz = dot (r, z);
+    double rrz = 0;
     for (it = 0; it < maxit; ++it)
       {
-        double rz = dot (r, z);
         if (fabs (rz) < min_division)
           return -1;
 
         mat_mult_vec (A, I, p, Avec);
-        apply_precond_J (A, Avec);
         double Apz = dot (Avec, z);
         if (fabs (Apz) < min_division)
           return -1;
 
         double alpha = rz / Apz;
         mult_sub_vec (q, u, alpha, Avec);
-        mult_sub_vec (u, u, -1, q);
-        mult_sub_vec (x, x, -alpha, u);
+        add_vec (u, q);
+        //mult_sub_vec (x, x, -alpha, u);
+        add_vec (x, u, alpha);
         mat_mult_vec (A, I, u, Avec);
-        apply_precond_J (A, Avec);
         mult_sub_vec (r, r, alpha, Avec);
         r_norm = sqrt (dot (r, r));
         if (r_norm < eps * rhs_norm)
           return it;
-        double rrz = dot (r, z);
+        rrz = dot (r, z);
         double betta = rrz / rz;
-        mult_sub_vec (u, r, -betta, q);
-        mult_sub_vec (Avec, q, -betta, p);
-        mult_sub_vec (p, u, -betta, Avec);
+        linear_combination (u, p, r, q, betta);
+        rz = rrz;
       }
     printf("r_norm = %e\n", r_norm / rhs_norm);
     return maxit;
